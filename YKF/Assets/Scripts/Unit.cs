bb6f6509d24658f05efadcee12e349ea6f68d0e7
alx168 <alx168@gmail.com>
2019-03-17 02:24:21 -0500

    Finished Unit generation

---
+++ YKF/Assets/Scripts/Unit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+	[SerializeField]
+    private GameObject UnitModel;
+    [SerializeField]
+    private int rank;
+    [SerializeField]
+    private int file;
+
+    public void generateSoldiers(GameObject prefab, int width, int height){
+    	for(int i = 0; i < width; i++){
+    		for(int j = 0; j < height; j++){
+    			GameObject go = Instantiate(prefab, 
+    				new Vector3(transform.position.x - (i * 1.5f), transform.position.y, transform.position.z - (j * 1.5f)), 
+    					Quaternion.identity) as GameObject;
+    			go.transform.parent = transform;
+    		}
+    	}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateSoldiers(UnitModel, rank, file);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
