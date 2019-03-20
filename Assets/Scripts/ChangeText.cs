a6a34a663d6dafd4f450f9b6b59095fca6f4005f
ehuynh96 <35850695+ehuynh96@users.noreply.github.com>
2019-03-19 21:05:30 -0500

    Add files via upload

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    public Text mytext = null;
+    public int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TextChange()
+    {
+        counter++;
+        if (counter % 2 == 1) {
+            mytext.text = "Computer's Turn";
+        }
+        else
+        {
+            mytext.text = "Player's Turn";
+        }
+
+    }
+}
