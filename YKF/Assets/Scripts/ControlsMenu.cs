fb48f351afcd1b5d157463dca2c801098c88377e
alx168 <alx168@gmail.com>
2019-04-20 23:31:55 -0500

    added controls menu

---
+++ YKF/Assets/Scripts/ControlsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject reminder;
+    [SerializeField]
+    GameObject text;
+    [SerializeField]
+    GameObject menuImage;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            reminder.SetActive(false);
+            menuImage.SetActive(true);
+            text.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Tab)) {
+            reminder.SetActive(true);
+            menuImage.SetActive(false);
+            text.SetActive(false);
+        }
+    }
+}
