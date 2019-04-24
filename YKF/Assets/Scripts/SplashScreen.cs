a37f19976b90d999eb5a6bd7505c277cd0e6181a
alx168 <alx168@gmail.com>
2019-04-23 20:11:51 -0500

    Eric's portion

---
+++ YKF/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static int SceneNumber;
+
+    void Start()
+    {
+        if(SceneNumber == 0)
+        {
+            StartCoroutine(ToSplashTwo());
+        }
+        if(SceneNumber == 1)
+        {
+            StartCoroutine(ToMain());
+        }
+    }
+
+    IEnumerator ToSplashTwo()
+    {
+        yield return new WaitForSeconds(5);
+        SceneNumber = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator ToMain()
+    {
+        yield return new WaitForSeconds(5);
+        SceneNumber = 2;
+        SceneManager.LoadScene(2);
+    }
+
+}
