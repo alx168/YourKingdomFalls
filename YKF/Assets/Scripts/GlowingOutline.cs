3bff1888ea023f0ddfbbd52462fb7f1e5bf1f645
alx168 <alx168@gmail.com>
2019-04-23 19:37:13 -0500

    outline shader

---
+++ YKF/Assets/Scripts/GlowingOutline.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingOutline : MonoBehaviour
+{
+    public Color _currentColor;
+    public Color _targetColor;
+    public float LerpFactor;
+    public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentColor = Color.Lerp(
+            _currentColor,
+            _targetColor,
+            Time.deltaTime * LerpFactor);
+        mat.SetColor("_GlowColor", _currentColor);
+        
+    }
+}
