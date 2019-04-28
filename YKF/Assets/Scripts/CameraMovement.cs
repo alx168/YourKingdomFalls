1946f426c27b009921ce88a609c137ce048e3c9e
alx168 <alx168@gmail.com>
2019-04-28 17:08:47 -0500

    Updated camera to support pitch control and boundaries

--- YKF/Assets/Scripts/CameraMovement.cs
+++ YKF/Assets/Scripts/CameraMovement.cs
@@ -8,44 +8,74 @@ public class CameraMovement : MonoBehaviour
     private float speedInput;
     private float doubleSpeed;
     private float speed;
+    [SerializeField]
+    private float MAX_X;
+    [SerializeField]
+    private float MIN_X;
+    [SerializeField]
+    private float MIN_Y;
+    [SerializeField]
+    private float MAX_Y;
+    [SerializeField]
+    private float MIN_Z;
+    [SerializeField]
+    private float MAX_Z;
+
     Transform t;
+    float pitch;
+    float yaw;
+    float roll;
 
     // Start is called before the first frame update
     void Start()
     {
         t = GetComponent<Transform>();
         doubleSpeed = speedInput * 2;
+        pitch = t.eulerAngles.x;
+        yaw = t.eulerAngles.y;
+        roll = t.eulerAngles.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftShift)){
+        Vector3 movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftShift)){
         	speed = doubleSpeed;
         }else{
         	speed = speedInput;
         }
         if(Input.GetKey("d")){
-        	t.Translate( new Vector3(speed * Time.deltaTime, 0, 0));
+            movement.x++;
         }
         if(Input.GetKey("a")){
-        	t.Translate( new Vector3(-speed * Time.deltaTime, 0, 0));
+            movement.x--;
         }
         if(Input.GetKey("w")){
-        	t.Translate( new Vector3(0, 0, speed * Time.deltaTime));
+            movement.z++;
         }
         if(Input.GetKey("s")){
-        	t.Translate( new Vector3(0, 0, -speed * Time.deltaTime));
+            movement.z--;
+        }
+        if(Input.GetKey("q"))
+        {
+            movement.y++;
+        }
+        if(Input.GetKey("e"))
+        {
+            movement.y--;
         }
         if(Input.GetMouseButton(1)){
-     		t.LookAt(t);
-         	t.RotateAround(t.position, Vector3.up, Input.GetAxis("Mouse X")*speed);
+            pitch += -speed * Input.GetAxis("Mouse Y");
+            yaw += speed * Input.GetAxis("Mouse X");
+            pitch = Mathf.Clamp(pitch, -10f, 60f);
+            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
         }
-        if(Input.GetKey("e") ){
-        	t.Translate( new Vector3(0, -speed * Time.deltaTime, 0));
-        }
-        if(Input.GetKey("q") ){
-        	t.Translate( new Vector3(0, speed * Time.deltaTime, 0));
-        }
+        t.Translate(movement * speed * Time.deltaTime, Space.Self);
+        transform.position = new Vector3(
+            Mathf.Clamp(t.position.x, MIN_X, MAX_X),
+            Mathf.Clamp(t.position.y, MIN_Y, MAX_Y),
+            Mathf.Clamp(t.position.z, MIN_Z, MAX_Z)
+        );
     }
 }
